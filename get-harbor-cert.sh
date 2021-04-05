2e9b4d9d2285a6b5040bc015422202dab3f6f309
Florian Casse <casse.florian@gmail.com>
2021-04-05 17:35:45 +0200

    Create get-harbor-cert.sh
    init

---
+++ get-harbor-cert.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# Ask for the Harbor FQDN and port
+echo "Enter the FQDN (e.g. harbor.florian.lab) of your Harbor registry:"
+
+read REGISTRY
+
+# Create folder for custom certificate as described in Docker docs https://docs.docker.com/engine/security/certificates/
+mkdir -p /etc/docker/certs.d/$REGISTRY
+
+# Download Registry Root Certificate
+wget -O /etc/docker/certs.d/$REGISTRY/ca.crt https://$REGISTRY/api/v2.0/systeminfo/getcert --no-check-certificate
